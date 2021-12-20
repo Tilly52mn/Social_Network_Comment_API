5675265934086675ef31424c23735079e1cd40b0
Adam Tilly <tillyjadam@gmail.com>
2021-12-19 23:09:14 -0600

    changed what is displayed when user, thoughts, and reactions are deleted

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -42,13 +42,12 @@ const thoughtController = {
       { $pull: { reactions: { reactionId: params.reactionId } } },
       { new: true }
     )
-      .then(dbUserData => res.json(dbUserData))
+      .then(dbUserData =>         res.json( { message: 'Reaction Deleted' }))
       .catch(err => res.json(err));
   },
   removeThought({ params }, res) {
     Thought.findOneAndDelete({ _id: params.thoughtId })
       .then(deletedThought => {
-        console.log(deletedThought)
         if (!deletedThought) {
           return res.status(404).json({ message: 'No thought with this id!' });
         }
@@ -59,16 +58,14 @@ const thoughtController = {
         );
       })
       .then(dbUserData => {
-        console.log(dbUserData)
         if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this id!' });
           return;
         }
-        res.json(dbUserData);
+        res.json( { message: 'Thought Deleted' });
       })
       .catch(err =>{
          res.json(err)
-         console.log(err)  
       });
   },
   getThoughtById({ params }, res) {
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -72,7 +72,8 @@ const userController = {
           res.status(404).json({ message: 'No user found with this id!' });
           return;
         }
-        res.json(dbUserData);
+        res.json( { message: 'User Deleted' })
+
       })
       .catch(err => res.status(400).json(err));
   },
